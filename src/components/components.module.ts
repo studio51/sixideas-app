150d4d8b395841f65a1e5f6a61835951fa45c7a1
pacMakaveli <pacMakaveli90@gmail.com>
2018-06-13 16:24:39 +0100

    chore(app): couple of changes; see commit

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -2,11 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserAvatarComponent } from './user-avatar/user-avatar';
+import { ContentDrawerComponent } from './content-drawer/content-drawer';
 
 @NgModule({
-	declarations: [UserAvatarComponent],
-	imports: [CommonModule],
-	exports: [UserAvatarComponent]
+	declarations: [
+    UserAvatarComponent,
+    ContentDrawerComponent
+  ],
+	
+  imports: [CommonModule],
+	
+  exports: [
+    UserAvatarComponent,
+    ContentDrawerComponent
+  ]
 })
 
 export class ComponentsModule { }
