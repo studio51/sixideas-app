adcf3f91b755ac948f6af7b2c29498257d4ef3a8
pacMakaveli <pacMakaveli90@gmail.com>
2018-06-13 16:23:07 +0100

    add(app): add directives module

---
+++ src/directives/directives.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { UnlessDirective } from './unless/unless';
+
+@NgModule({
+	declarations: [UnlessDirective],
+	imports: [],
+	exports: [UnlessDirective]
+})
+
+export class DirectivesModule { }
