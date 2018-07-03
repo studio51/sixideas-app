da754afb691fa4678aab492b76d31da0c66f0c7f
pacMakaveli <pacMakaveli90@gmail.com>
2018-07-03 14:54:33 +0100

    udpate(community): api updates and additions

--- src/pages/community/community.ts
+++ src/pages/community/community.ts
@@ -8,6 +8,9 @@ import { Camera, CameraOptions } from '@ionic-native/camera';
 import { User } from '../../models/user';
 import { Post } from '../../models/post';
 
+import { UserProvider } from '../../providers/user';
+import { PostProvider } from '../../providers/post';
+
 @IonicPage()
 @Component({
   selector: 'page-community',
@@ -18,36 +21,28 @@ export class CommunityPage {
   @ViewChild('postFormTitle') postFormTitle: ElementRef;
   @ViewChild('postFormImageOptions') postFormImageOptions: ElementRef;
 
-  currentUser: User;
+  user: User;
   posts: Post[] = [];
 
   newPostFormIsActive: boolean = false;
  
-  drawerOptions: any;
-
   postColours: Array<{
     name: string,
     hex: string 
   }>;
 
   postForm: FormGroup;
-  commentForm: FormGroup;
 
   constructor(
     public camera: Camera,
-    public navCtrl: NavController,
-    public navParams: NavParams,
-    public actionSheetCtrl: ActionSheetController
+    // public navCtrl: NavController,
+    // public navParams: NavParams,
+    public actionSheetCtrl: ActionSheetController,
+    public userProvider: UserProvider,
+    public postProvider: PostProvider
   
   ) {
 
-    this.drawerOptions = {
-      handleHeight: 50,
-      thresholdFromBottom: 200,
-      thresholdFromTop: 200,
-      bounceBack: true
-    };
- 
     this.postColours = [
       { name: 'Default', hex: '#ffffff' },
       { name: 'Blue',    hex: '#007bff' },
@@ -56,126 +51,6 @@ export class CommunityPage {
       { name: 'Red',     hex: '#dc3545' }
     ]
 
-    this.currentUser = {
-      id: 2,
-      name: "Vlad Radulescu",
-      background: "https://res.cloudinary.com/dwvwdhzvg/image/upload/c_fill,h_270,w_1440/nciqyiykd7fya5c9ul0d",
-      avatar: "https://randomuser.me/api/portraits/lego/6.jpg",
-      colour: "#195097",
-      tag: "@pacMakaveli",
-      bio: "",
-      following: [],
-      followers: [{ id: 1 }, { id: 3 }],
-      likes: [],
-      interests: [
-        { name: 'Design' },
-        { name: 'Graphics' },
-        { name: 'Technology' },
-        { name: 'Health' },
-        { name: 'EducatingTheFuture' },
-        { name: 'BookClub' }
-      ],
-      online: true
-    }
-
-    const posts = [
-      {
-        id: 1,
-        title: "",
-        body: "Hello World! Let's start with explicitly saying that what you see is not what you get, in terms of UI. The MVP, which you are now browsing, is using default Ionic elements that might not match the web version. The UI will change drastically, in places, once the functionality is in.",
-        author: {
-          id: 2,
-          name: "Vlad Radulescu",
-          avatar: "https://randomuser.me/api/portraits/lego/6.jpg",
-          colour: "#195097"
-        },
-        comments: [
-          {
-            id: 1,
-            body: "This is a Comment",
-            author: {
-              id: 2,
-              name: "Vlad Radulescu",
-              avatar: "https://randomuser.me/api/portraits/lego/6.jpg",
-              colour: "#195097"
-            },
-            likes_count: 2
-          },
-          {
-            id: 2,
-            body: "This is another Comment. But unlike the first one, this one spans across multiple rows.",
-            author: {
-              id: 2,
-              name: "Vlad Radulescu",
-              avatar: "https://randomuser.me/api/portraits/lego/6.jpg",
-              colour: "#195097"
-            },
-            likes_count: 0
-          },
-          {
-            id: 3,
-            body: "Foo Bar Daz",
-            author: {
-              id: 3,
-              name: "Richard Adams",
-              avatar: "https://randomuser.me/api/portraits/lego/7.jpg",
-              colour: "#195097"
-            },
-            likes_count: 0
-          }
-        ],
-        created_at: "",
-        likes_count: 3
-      },
-      {
-        id: 2,
-        title: "Did you know?",
-        body: "Just because there's no backend, doesn't mean you can't leave comments! Go on, try it now :) ( they will not persist )",
-        author: {
-          id: 2,
-          name: "Vlad Radulescu",
-          avatar: "https://randomuser.me/api/portraits/lego/6.jpg",
-          colour: "#195097"
-        },
-        comments: [],
-        created_at: "",
-        likes_count: 3
-      },
-      {
-        id: 3,
-        title: "",
-        body: "@usable a quick poll reveals that most of the users access the community via a mobile app and are on iphones. Interestingly, the android users also use the desktop...and a couple have both iphone and android/ windows. I think it is going to be important to have the iphone version mobile to keep the levels of engagement up...particularly with badge icon notification.",
-        author: {
-          id: 3,
-          name: "Richard Adams",
-          avatar: "https://randomuser.me/api/portraits/lego/7.jpg",
-          colour: "#195097"
-        },
-        comments: [],
-        created_at: "",
-        likes_count: 3
-      },
-      {
-        id: 4,
-        title: "",
-        body: "Look at me! I'm blue(ish)",
-        background: '#007bff',
-        author: {
-          id: 3,
-          name: "Richard Adams",
-          avatar: "https://randomuser.me/api/portraits/lego/7.jpg",
-          colour: "#195097"
-        },
-        comments: [],
-        created_at: "",
-        likes_count: 3
-      },
-    ];
-
-    for (let post of posts) {
-      this.posts.push(new Post(post));
-    }
-
     this.postForm = new FormGroup({
       body: new FormControl('', Validators.required),
 
@@ -183,34 +58,37 @@ export class CommunityPage {
       colour: new FormControl(),
       cover:  new FormControl()
     })
+  }
 
-    this.commentForm = new FormGroup({
-      body: new FormControl('', Validators.required)
+  ionViewDidEnter() {
+    this.userProvider.get('5b374f7f96e80db323df2942').subscribe((user: User) => {
+      this.user = user;
+      this.getPosts();
     })
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CommunityPage');
+  private getPosts() {
+    this.postProvider.load('', { include_author: true }).subscribe((posts: Post[]) => {
+      console.log('community', posts[0])
+      console.log('community', posts[1])
+      
+      this.posts = posts
+    })
   }
 
-  showNewFormPost() {
+  showNewPostForm() {
     this.newPostFormIsActive = true;
   }
 
-  likePost(postID: number) {
-    const post = this.posts.find((post: Post) => post.id === postID);
-          
-          post.likes_count = post.likes_count + 1;
-  }
-
-  likeComment(postID: number, commentID: number) {
-    const post = this.posts.find((post: Post) => post.id === postID),
-          comment: any = post.comments.find((comment: any) => comment.id === commentID);
-
-          comment.likes_count = comment.likes_count + 1;
-  }
-
   changePostFormColour(colourHEX: any) {
+    let a: any = this.postFormTitle;
+
+    if (colourHEX === "#ffffff") {
+      a.setElementStyle('display', 'block')
+    } else {
+      a.setElementStyle('display', 'none')
+    }
+
     this.postForm.controls.colour.setValue(colourHEX)
   }
 
@@ -266,25 +144,16 @@ export class CommunityPage {
   }
 
   addPost() {
+    console.log(this.postForm)
+
     const post: any = {}; //new Post();
 
           post.title  = this.postForm.value.title;
           post.body   = this.postForm.value.body;
-          post.colour = this.postForm.value.colour;
-          post.author = this.currentUser;
+          post.background = this.postForm.value.colour;
 
-    this.posts.push(post)
-  }
-
-  addComment(postID: number) {    
-    const comment: any = {};
-          comment.body = this.commentForm.value.body;
-          comment.author = this.currentUser;
-
-    const post = this.posts.find((post: Post) => post.id === postID);
-      console.log(post)
-
-          post.comments.push(comment)
-
+    this.posts.push(post);
+    this.postForm.reset();
+    this.newPostFormIsActive = false;
   }
 }
