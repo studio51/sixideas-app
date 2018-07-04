b61d2731f22d0c37928e73ece69100973f519838
pacMakaveli <pacMakaveli90@gmail.com>
2018-07-04 12:43:01 +0100

    update(community): add type of posts through feed; update the colour scheme to reflect the current user and clean up old code

--- src/pages/community/community.ts
+++ src/pages/community/community.ts
@@ -1,9 +1,5 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
-import { IonicPage, NavController, NavParams, ActionSheetController } from 'ionic-angular';
-
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-
-import { Camera, CameraOptions } from '@ionic-native/camera';
+import { Component } from '@angular/core';
+import { IonicPage, ModalController } from 'ionic-angular';
 
 import { User } from '../../models/user';
 import { Post } from '../../models/post';
@@ -18,139 +14,54 @@ import { PostProvider } from '../../providers/post';
 })
 
 export class CommunityPage {
-  @ViewChild('postFormTitle') postFormTitle: ElementRef;
-  @ViewChild('postFormImageOptions') postFormImageOptions: ElementRef;
-
   user: User;
   posts: Post[] = [];
 
-  newPostFormIsActive: boolean = false;
- 
-  postColours: Array<{
-    name: string,
-    hex: string 
-  }>;
+  showLoadingIndicator: boolean = true;
 
-  postForm: FormGroup;
+  feed: string = 'community';
 
   constructor(
-    public camera: Camera,
-    // public navCtrl: NavController,
-    // public navParams: NavParams,
-    public actionSheetCtrl: ActionSheetController,
+    public modalCtrl: ModalController,
+    public storage: Storage,
     public userProvider: UserProvider,
     public postProvider: PostProvider
   
-  ) {
-
-    this.postColours = [
-      { name: 'Default', hex: '#ffffff' },
-      { name: 'Blue',    hex: '#007bff' },
-      { name: 'Green',   hex: '#28a745' },
-      { name: 'Yellow',  hex: '#ffc107' },
-      { name: 'Red',     hex: '#dc3545' }
-    ]
-
-    this.postForm = new FormGroup({
-      body: new FormControl('', Validators.required),
-
-      title:  new FormControl(),
-      colour: new FormControl(),
-      cover:  new FormControl()
-    })
-  }
+  ) { }
 
   ionViewDidEnter() {
-    this.userProvider.get('5b374f7f96e80db323df2942').subscribe((user: User) => {
+    this.userProvider.get().subscribe((user: User) => {
       this.user = user;
+
       this.getPosts();
     })
   }
 
-  private getPosts() {
-    this.postProvider.load('', { include_author: true }).subscribe((posts: Post[]) => {
-      this.posts = posts
-    })
+  public feedChanged(event: any) {
+    this.showLoadingIndicator = true;
+    this.getPosts(event.value)
   }
 
-  showNewPostForm() {
-    this.newPostFormIsActive = true;
-  }
-
-  changePostFormColour(colourHEX: any) {
-    let a: any = this.postFormTitle;
-
-    if (colourHEX === "#ffffff") {
-      a.setElementStyle('display', 'block')
-    } else {
-      a.setElementStyle('display', 'none')
-    }
-
-    this.postForm.controls.colour.setValue(colourHEX)
-  }
-
-  showPostFormImageOptions() {
-    const buttons: Array<any> = [];
-
-    buttons.push({
-      text: 'Camera',
-      handler: () => { this.captureImage('CAMERA') }
-    })
+  private getPosts(feed?: string) {
+    const params = {}
+          params['include_author'] = true
     
-    buttons.push({
-      text: 'Library',
-      handler: () => { this.captureImage('Library') }
+    if (feed && feed != 'community') { params['feed'] = feed }
+
+    this.postProvider.load('', params).subscribe((posts: Post[]) => {
+      this.posts = posts
+      this.showLoadingIndicator = false;
     })
-
-    buttons.push({
-      text: 'Cancel',
-      role: 'cancel'
-    });
-
-    let actionSheet = this.actionSheetCtrl.create({
-      title: 'Asset Actions',
-      buttons: buttons
-    });
-
-    actionSheet.present();
   }
 
-  private captureImage(source: string) {
-    const options: CameraOptions = {
-      
-                 quality: 100,
-        saveToPhotoAlbum: (source == 'CAMERA'),
-               allowEdit: false,
-      correctOrientation: true,
-         destinationType: this.camera.DestinationType.FILE_URI,
-              sourceType: (source == 'CAMERA' ? this.camera.PictureSourceType.CAMERA : this.camera.PictureSourceType.PHOTOLIBRARY),
-            encodingType: this.camera.EncodingType.JPEG,
-               mediaType: this.camera.MediaType.PICTURE
-    }
-
-    this.camera.getPicture(options).then((image) => {
-      this.postForm.controls.cover.setValue(image);
-    }, (error) => {
-      console.info('');
-      console.error(error);
-
-      if (error !== 'no image selected' || error !== 'No camera available') {
-        console.error(error)
+  public newPost() {
+    const postFormModal = this.modalCtrl.create('PostFormPage');
+  
+    postFormModal.present();
+    postFormModal.onDidDismiss((post: Post) => {
+      if (post) {
+        this.posts.push(post)
       }
     })
-  }
-
-  addPost() {
-    console.log(this.postForm)
-
-    const post: any = {}; //new Post();
-
-          post.title  = this.postForm.value.title;
-          post.body   = this.postForm.value.body;
-          post.background = this.postForm.value.colour;
-
-    this.posts.push(post);
-    this.postForm.reset();
-    this.newPostFormIsActive = false;
   }
 }
