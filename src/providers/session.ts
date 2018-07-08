3b9c0f7c553f9dbc34fd323892ebb4f126271c76
pacMakaveli <pacMakaveli90@gmail.com>
2018-07-08 03:30:24 +0100

    feature(app/session): move 'session' functions and routes into its own provider

---
+++ src/providers/session.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { Platform } from 'ionic-angular';
+
+import { Device } from '@ionic-native/device';
+
+import { SixIdeasHTTPService } from '../services/http';
+import { NotificationService } from '../services/notification';
+
+import { User } from '../models/user';
+
+@Injectable()
+export class SessionProvider {
+  constructor(
+    public platform: Platform,
+    public device: Device,
+    public http: SixIdeasHTTPService,
+    public notificationService: NotificationService
+  
+  ) { }
+
+  public authenticate(credentials?: Object) {
+    const device: Object = {};
+          device['device'] = {}
+    
+    if (this.platform.is('cordova')) {
+      device['device'] = {
+        platform: this.device.platform,
+        model: this.device.model,
+        uuid: this.device.uuid
+      }
+    }
+
+    return this.notificationService.register().flatMap((token: any) => {
+      if (this.platform.is('cordova')) {
+        device['device']['token'] = token.registrationId;
+        device['device']['type'] = token.registrationType;
+
+        console.log(JSON.stringify(Object.assign(credentials, device)));
+      }
+
+      return this.http.post('sessions/authenticate', Object.assign(credentials, device))
+    })
+  }
+
+  public user() {
+    return this.http.get('sessions/user').map((response: any) => response.success ? response.user : response)
+  }
+
+  public logout() {
+    return this.http.delete('sessions/logout', {})
+  }
+
+  public appear() {
+    return this.http.post('sessions/appear', {})
+  }
+
+  public away() {
+    return this.http.post('sessions/away', {})
+  }
+
+  private dissapear() {
+    return this.http.post('sessions/disappear', {})
+  }
+}
