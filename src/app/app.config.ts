057ad808f288b13045e4d55db5fd73aa47b09258
Vladut Radulescu <pacMakaveli90@gmail.com>
2019-05-17 16:14:54 +0100

    push everything

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,9 @@
 enum endpoints {
   production  = 'https://1801-six-ideas.mdw.re',
-  development = 'http://192.168.1.9:3000'
+  development = 'http://192.168.0.11:3000'
 }
 
-const environment: string = 'production';
+const environment: string = 'development';
 
 export const webURL: string = `${ endpoints[environment] }`;
 export const url: string = `${ webURL }/api/v1/`;
