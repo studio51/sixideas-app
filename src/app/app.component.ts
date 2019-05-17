14ef1537935e1202dcbba3d8708e721421ba3927
Vladut Radulescu <pacMakaveli90@gmail.com>
2019-05-17 16:12:39 +0100

    update(app): config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,23 +4,59 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { User } from './interfaces/user';
+
+import { UserProvider } from './providers/user';
+import { CableProvider } from 'src/app/providers/cable';
+import { AppereanceService } from './services/appearance.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
+
 export class AppComponent {
+  user: User;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public userProvider: UserProvider,
+    public cableProvider: CableProvider,
+    private appereanceService: AppereanceService
+
   ) {
+
     this.initializeApp();
   }
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+  async initializeApp() {
+    await this.platform.ready();
+
+    this.user = await this.userProvider.current();
+
+    this.statusBar.styleDefault();
+    this.splashScreen.hide();
+
+    this.subscribeToAppearances();
+    this.subscribeToNewPosts();
+  }
+
+  private async subscribeToAppearances() {
+    this.cableProvider.appearances().subscribe((appearances: any) => {
+      Object.keys(appearances).forEach((uuid: string) => {
+        this.appereanceService.change.next({
+          uuid: uuid,
+          state: appearances[uuid]
+        });
+      });
+    });
+  }
+
+  private async subscribeToNewPosts() {
+    this.cableProvider.posts().subscribe((counter: any) => {
+      console.log(counter)
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,81 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicStorageModule } from '@ionic/storage';
+
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Camera } from '@ionic-native/camera/ngx';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { WebView } from '@ionic-native/ionic-webview/ngx';
+import { File } from '@ionic-native/file/ngx';
+import { FileTransfer } from '@ionic-native/file-transfer/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HTTPInterceptor } from './services/http.interceptor';
+
+import { ComponentsModule } from './components/components.module';
+
+import { UserPageModule } from './pages/user/user.module';
+import { UserCardPageModule } from './pages/user-card/user-card.module';
+import { UserEditPageModule } from './pages/user-edit/user-edit.module';
+import { PostPageModule } from './pages/post/post.module';
+import { PostFormPageModule } from './pages/post-form/post-form.module';
+import { TagsPageModule } from './pages/tags/tags.module';
+
+import { ActionCableService } from 'angular2-actioncable';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+  ],
+
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    IonicModule.forRoot(),
+    IonicStorageModule.forRoot(),
+    AppRoutingModule,
+    ComponentsModule,
+    UserPageModule,
+    UserCardPageModule,
+    UserEditPageModule,
+    PostPageModule,
+    PostFormPageModule,
+    TagsPageModule
+  ],
+
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    Camera,
+    InAppBrowser,
+    WebView,
+    File,
+    FileTransfer,
+
+    {
+      provide: RouteReuseStrategy,
+      useClass: IonicRouteStrategy
+    },
+
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HTTPInterceptor,
+      multi: true
+    },
+
+    ActionCableService
   ],
+
   bootstrap: [AppComponent]
 })
+
 export class AppModule {}
