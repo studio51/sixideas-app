c42d3d3dbc63e14a401c5b2997768c5040319575
Vladut Radulescu <pacMakaveli90@gmail.com>
2019-05-21 11:37:16 +0100

    update(notifications): finish up the Notifications page including markRead() and refresh()

--- src/app/pages/notifications/notifications.page.ts
+++ src/app/pages/notifications/notifications.page.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
-import { User } from 'src/app/interfaces/user';
+import { AlertController, ModalController } from '@ionic/angular';
+
 import { Notification } from 'src/app/interfaces/notification';
+import { User } from 'src/app/interfaces/user';
 
 import { UserProvider } from 'src/app/providers/user';
 import { NotificationProvider } from 'src/app/providers/notification';
+
+import { PostPage } from '../post/post.page';
 
 // 3rd party plugins
 //
@@ -16,9 +20,9 @@ import * as Sugar from 'sugar';
 // make it available everywhere.
 //
 Sugar.extend({
-  namespaces: [Date],
-  methods: ['relative', 'format']
-})
+  namespaces: [String, Date],
+  methods: ['truncate', 'relative', 'format']
+});
 
 @Component({
   selector: 'app-notifications',
@@ -27,33 +31,91 @@ Sugar.extend({
 })
 
 export class NotificationsPage implements OnInit {
-
   user: User;
   notifications: Notification[] = [];
 
   constructor(
     public userProvider: UserProvider,
-    public notificationsProvider: NotificationProvider
+    public notificationsProvider: NotificationProvider,
+    public modalCtrl: ModalController,
+    public alertCtrl: AlertController
 
   ) { }
 
   async ngOnInit() {
     this.user = await this.userProvider.current();
-
     this.get();
   }
 
-  public async get() {
+  public async get(event?: any) {
     this.notifications = await this.notificationsProvider.load();
-  }
 
-  public async markAllAsRead() {
-    await this.notificationsProvider.read();
-    this.get();
+    if (event) {
+      await event.target.complete();
+    }
   }
 
   public timeAgoInWords(date: string): string {
     // @ts-ignore
     return new Date(date).relative();
+  }
+
+  public async confirm() {
+    const alert = await this.alertCtrl.create({
+      header: 'Are you sure?',
+      message: 'This will remove all of your Notifications!',
+      buttons: [{
+        text: 'Cancel',
+        role: 'cancel',
+        cssClass: 'secondary'
+      }, {
+        text: 'Okay',
+        handler: () => {
+          this.markAllAsRead();
+        }
+      }]
+    });
+
+    await alert.present();
+  }
+
+  public getAndViewObject(event: any, notification: Notification) {
+    const object: any = notification.object_with_usernames;
+    let postID: string,
+        commentID: string = null;
+
+    switch (notification.type) {
+      case 'comment':
+      case 'comment_like':
+        postID    = object.commentable_id.$oid;
+        commentID = notification.scope_id.$oid;
+
+        break;
+      case 'post':
+      case 'post_like':
+        postID    = notification.scope_id.$oid;
+
+        break;
+    }
+
+    this.viewPost(postID, commentID);
+  }
+
+  private async viewPost(id: string, commentID?: string) {
+    const modal: any = await this.modalCtrl.create({
+      component: PostPage,
+      componentProps: {
+        id: id,
+        commentID: commentID,
+        user: this.user
+      }
+    });
+
+    return await modal.present();
+  }
+
+  private async markAllAsRead() {
+    await this.notificationsProvider.read();
+    this.get();
   }
 }
