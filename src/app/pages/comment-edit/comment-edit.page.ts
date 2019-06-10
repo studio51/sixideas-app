93a8126868919228a4010ca0d6f24c92461d76bb
Vladut Radulescu <pacMakaveli90@gmail.com>
2019-06-10 14:52:07 +0100

    feature(comment): add CommentEdit module

---
+++ src/app/pages/comment-edit/comment-edit.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comment-edit',
+  templateUrl: './comment-edit.page.html',
+  styleUrls: ['./comment-edit.page.scss'],
+})
+export class CommentEditPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
